d876e11bed1dc7d61396f614a79d3a121c789075
VanLocNguyen1999 <locnv@selex.vn>
2024-09-18 14:22:03 +0700

    fix link

--- uukanshu/src/home.js
+++ uukanshu/src/home.js
@@ -8,11 +8,11 @@ function execute() {
         {title: "Đô Thị", input: class_3 , script: "gen.js"},    
         {title: "Lịch Sử Quân Sự", input: class_4  , script: "gen.js"}, 
         {title: "Khoa Học Huyễn Tưởng", input: class_5  , script: "gen.js"},
-        {title: "Trò Chơi", input: BASE_URL + class_6  , script: "gen.js"},    
-        {title: "Khinh Dị", input: BASE_URL + class_7  , script: "gen.js"},
-        {title: "Lãng Mạn", input: BASE_URL + class_8  , script: "gen.js"},  
-        {title: "Anime", input: BASE_URL + class_9  , script: "gen.js"},
-        {title: "Thể Loại Khác", input: BASE_URL + class_10  , script: "gen.js"}, 
+        {title: "Trò Chơi", input:  class_6  , script: "gen.js"},    
+        {title: "Khinh Dị", input:  class_7  , script: "gen.js"},
+        {title: "Lãng Mạn", input:  class_8  , script: "gen.js"},  
+        {title: "Anime", input:  class_9  , script: "gen.js"},
+        {title: "Thể Loại Khác", input: class_10  , script: "gen.js"}, 
     ]);
 }
 
