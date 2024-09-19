2ef8c5a330fd9fc51e7cea7f8af73caf4ea2102f
VanLocNguyen1999 <locnv@selex.vn>
2024-09-19 14:39:53 +0700

    aa

--- uukanshu/src/gen.js
+++ uukanshu/src/gen.js
@@ -1,20 +1,22 @@
 load('config.js');
 
 function execute(url, page) {
-    if (!page) page = '1';
+    if (!page) {
+        page = '1';
+    }
 
-    let response = fetch(BASE_URL + url + '_' + page + '.html');
+    let response = fetch(url.replace("1.html", page + ".html"));
 
     if (response.ok) {
         let doc = response.html();
         var booklist = [];
-        var next = doc.select(".pagelink").select(">strong").text() + 1 ;
+        var next = doc.select("div#pagelink.pagelik").select(">strong").text() ;
         let img = 'https://raw.githubusercontent.com/VanLocNguyen1999/vbook/refs/heads/Loc_nv/uukanshu/icon.png' ;
-        doc.select(".content book bookbox").forEach(e => {
+        doc.select("div#fengtui.content.book").forEach(e => {
             booklist.push({
-                name: e.select(".bookname > a").text(),
-                link: e.select(".bookname > a").attr("href"),
-                description: e.select(".bookinfo .update").html(),
+                name: e.select("div.bookbox").select("h4.bookname > a").text(),
+                link: e.select("div.bookbox").select("h4.bookname > a").attr("href"),
+                description: e.select("div.bookbox").select(".bookinfo .update").html(),
                 cover: img,
                 host: BASE_URL,
             });
--- uukanshu/src/home.js
+++ uukanshu/src/home.js
@@ -3,16 +3,16 @@ load('config.js');
 function execute() {
     return Response.success([
 
-        {title: "Huyền Huyễn", input: 'class_1', script: "gen.js"}, 
-        {title: "Võ Hiệp", input: 'class_2' , script: "gen.js"},
-        {title: "Đô Thị", input: 'class_3' , script: "gen.js"},    
-        {title: "Lịch Sử Quân Sự", input: 'class_4'  , script: "gen.js"}, 
-        {title: "Khoa Học Huyễn Tưởng", input: 'class_5'  , script: "gen.js"},
-        {title: "Trò Chơi", input:  'class_6'  , script: "gen.js"},    
-        {title: "Khinh Dị", input:  'class_7'  , script: "gen.js"},
-        {title: "Lãng Mạn", input:  'class_8'  , script: "gen.js"},  
-        {title: "Anime", input:  'class_9'  , script: "gen.js"},
-        {title: "Thể Loại Khác", input: 'class_10'  , script: "gen.js"}, 
+        {title: "Huyền Huyễn", input: "https://uukanshu.cc/class_1_1.html", script: "gen.js"}, 
+        // {title: "Võ Hiệp", input: 'class_2' , script: "gen.js"},
+        // {title: "Đô Thị", input: 'class_3' , script: "gen.js"},    
+        // {title: "Lịch Sử Quân Sự", input: 'class_4'  , script: "gen.js"}, 
+        // {title: "Khoa Học Huyễn Tưởng", input: 'class_5'  , script: "gen.js"},
+        // {title: "Trò Chơi", input:  'class_6'  , script: "gen.js"},    
+        // {title: "Khinh Dị", input:  'class_7'  , script: "gen.js"},
+        // {title: "Lãng Mạn", input:  'class_8'  , script: "gen.js"},  
+        // {title: "Anime", input:  'class_9'  , script: "gen.js"},
+        // {title: "Thể Loại Khác", input: 'class_10'  , script: "gen.js"}, 
     ]);
 }
 
