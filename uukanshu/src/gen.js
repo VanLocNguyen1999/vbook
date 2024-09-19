c5c08bac147ed831455f4e2d9f10c596c9a44bf5
VanLocNguyen1999 <locnv@selex.vn>
2024-09-19 14:16:33 +0700

    aaa

--- uukanshu/src/gen.js
+++ uukanshu/src/gen.js
@@ -1,25 +1,29 @@
 load('config.js');
 
-function execute(url, page) {
+async function execute(url, page) {
     if (!page) page = '1';
 
-    let response = fetch(BASE_URL + url + '_' + page + '.html');
+    let response = await fetch(BASE_URL + url + '_' + page + '.html');
 
     if (response.ok) {
-        let doc = response.html();
+        let html = await response.text();
+        let doc = new DOMParser().parseFromString(html, 'text/html');
+        
         var booklist = [];
-        var next = doc.select(".pagelink").select(">strong").text() + 1 ;
-        let img = 'https://raw.githubusercontent.com/VanLocNguyen1999/vbook/refs/heads/Loc_nv/uukanshu/icon.png' ;
-        doc.select(".content book bookbox").forEach(e => {
+        var next = parseInt(doc.querySelector(".pagelink strong").textContent) + 1;
+        let img = 'https://raw.githubusercontent.com/VanLocNguyen1999/vbook/refs/heads/Loc_nv/uukanshu/icon.png';
+        
+        doc.querySelectorAll(".content .book .bookbox").forEach(e => {
             booklist.push({
-                name: e.select(".bookname > a").text(),
-                link: e.select(".bookname > a").attr("href"),
-                description: e.select(".bookinfo update").html(),
+                name: e.querySelector(".bookname > a").textContent,
+                link: e.querySelector(".bookname > a").getAttribute("href"),
+                description: e.querySelector(".bookinfo .update").innerHTML,
                 cover: img,
                 host: BASE_URL,
             });
         });
         return response.success(booklist, next);
-        }
+    }
     return null;
 }
+
