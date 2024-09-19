eb1bdccde380e08ee445afede1edac5c62503a05
VanLocNguyen1999 <locnv@selex.vn>
2024-09-19 11:45:35 +0700

    fix

--- uukanshu/src/gen.js
+++ uukanshu/src/gen.js
@@ -3,7 +3,7 @@ load('config.js');
 function execute(url, page) {
     if (!page) page = '1';
 
-    let response = fetch(BASE_URL + url + "_" + page + ".html");
+    let response = fetch(BASE_URL + url + '_' + page + '.html');
 
     if (response.ok) {
         let doc = response.html();
