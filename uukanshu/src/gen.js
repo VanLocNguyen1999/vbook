162021267e93e4d7f93c036b39618d0ad3e26fc0
VanLocNguyen1999 <locnv@selex.vn>
2024-09-19 15:04:29 +0700

    aa

--- uukanshu/src/gen.js
+++ uukanshu/src/gen.js
@@ -5,19 +5,16 @@ function execute(url, page) {
         page = '1';
     }
 
-    let response = fetch(url.replace("1.html", page + ".html"));
+    let response = fetch('https://uukanshu.cc/class_1_1.html');
 
     if (response.ok) {
         let doc = response.html();
         var booklist = [];
-        var next = doc.select("div#pagelink.pagelik").select(">strong").text() ;
-        let img = 'https://raw.githubusercontent.com/VanLocNguyen1999/vbook/refs/heads/Loc_nv/uukanshu/icon.png' ;
         doc.select("div#fengtui.content.book").forEach(e => {
             booklist.push({
                 name: e.select("div.bookbox").select("h4.bookname > a").text(),
                 link: e.select("div.bookbox").select("h4.bookname > a").attr("href"),
                 description: e.select("div.bookbox").select(".bookinfo .update").html(),
-                cover: img,
                 host: BASE_URL,
             });
         });
