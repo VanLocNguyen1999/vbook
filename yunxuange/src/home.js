d67020a2ffee0ec2c9a31b4c9fe793882acf0961
VanLocNguyen1999 <locnv@selex.vn>
2024-09-28 17:27:13 +0700

    add

--- yunxuange/src/home.js
+++ yunxuange/src/home.js
@@ -1,6 +1,6 @@
 load('config.js');
 function execute() {
     return Response.success([
-        {title: "Võ Hiệp Tiên Hiệp", input: "/sort_wuxia", script: "gen.js"},
+        {title: "Võ Hiệp Tiên Hiệp", input: "/sort_wuxia_", script: "gen.js"},
     ]);
 }
